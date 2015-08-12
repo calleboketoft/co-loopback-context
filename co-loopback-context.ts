e7c978f5a8ddeebda9fc3ffe1031b994ff675857
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-08-12 14:26:53 +0200

    initial commit

---
+++ co-loopback-context.ts
@@ -0,0 +1,26 @@
+// Makes it possible to access currently authenticated user
+// https://github.com/strongloop/loopback/issues/569#issuecomment-62690674
+// var ctx = loopback.getCurrentContext();
+// var currentUser = ctx && ctx.get('currentUser');
+
+// Put it in the file 'server.js' here:
+// -- Add your pre-processing middleware here --
+// require('../co-loopback-context')(app)
+
+var loopback = require('loopback')
+
+module.exports = function (app) {
+  app.use(loopback.context())
+  app.use(loopback.token())
+  app.use((req, res, next) => {
+    if (!req.accessToken) return next()
+    app.models.User.findById(req.accessToken.userId, (err, user) => {
+      if (err) return next(err)
+      if (!user) return next(new Error('No user with this access token was found.'))
+      res.locals.currentUser = user
+      var loopbackContext = loopback.getCurrentContext()
+      if (loopbackContext) loopbackContext.set('currentUser', user)
+      next()
+    })
+  })
+}
